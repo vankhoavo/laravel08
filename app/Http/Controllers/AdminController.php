3198a7cdebc8afd0a489577458bb11117b2cbb31
vankhoavo <vovankhoa2001@hotmail.com>
2023-03-06 00:28:45 +0700

    testextension

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,13 +27,13 @@ class AdminController extends Controller
 
     public function create(CreateTaiKhoanAdminRequest $request)
     {
-        $data               = $request->all();
-        $data['password']   = bcrypt($request->password);
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
         Admin::create($data);
 
         return response()->json([
-            'status'    => 1,
-            'mess'      => 'Đã tạo tài khoản thành công!',
+            'status' => 1,
+            'mess' => 'Đã tạo tài khoản thành công!',
         ]);
     }
 
@@ -41,18 +41,18 @@ class AdminController extends Controller
     {
         $admin = Admin::where('id', $request->id)->first();
 
-        if($admin) {
+        if ($admin) {
             $admin->is_block = !$admin->is_block;
             $admin->save();
 
             return response()->json([
-                'status'    => 1,
-                'mess'      => 'Đã đổi trạng thái tài khoản ' . $admin->ho_va_ten,
+                'status' => 1,
+                'mess' => 'Đã đổi trạng thái tài khoản ' . $admin->ho_va_ten,
             ]);
         } else {
             return response()->json([
-                'status'    => 0,
-                'mess'      => 'Quậy vừa vừa thôi',
+                'status' => 0,
+                'mess' => 'Quậy vừa vừa thôi',
             ]);
         }
     }
@@ -65,8 +65,8 @@ class AdminController extends Controller
         $admin->update($request->all());
 
         return response()->json([
-            'status'    => 1,
-            'mess'      => 'Đã cập nhật tài khoản ' . $admin->email,
+            'status' => 1,
+            'mess' => 'Đã cập nhật tài khoản ' . $admin->email,
         ]);
     }
 
@@ -77,17 +77,17 @@ class AdminController extends Controller
 
     public function actionLogin(Request $request)
     {
-        $data  = $request->all();
+        $data = $request->all();
         $check = Auth::guard('admin')->attempt($data);
-        if($check) {
+        if ($check) {
             return response()->json([
-                'status'    => true,
-                'mess'      => 'Đã login thành công!',
+                'status' => true,
+                'mess' => 'Đã login thành công!',
             ]);
         } else {
             return response()->json([
-                'status'    => false,
-                'mess'      => 'Tài khoản hoặc mật khẩu không đúng!',
+                'status' => false,
+                'mess' => 'Tài khoản hoặc mật khẩu không đúng!',
             ]);
         }
     }
